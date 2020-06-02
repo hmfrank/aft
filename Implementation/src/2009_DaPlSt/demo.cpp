8f78aa6e87fb9a2fd9853c04110b72b68ae7f0c7
Hannes Frank <hannes.frank@mailbox.tu-dresden.de>
2020-06-02 05:46:09 +0200

    Tidy Up

--- Implementation/src/2009_DaPlSt/demo.cpp
+++ Implementation/src/2009_DaPlSt/demo.cpp
@@ -2,8 +2,6 @@
 
 #include "2009_DaPlSt/2009_DaPlSt.h"
 #include <cassert>
-#include <Gamma/AudioIO.h>
-#include <iostream>
 #include "misc.h"
 #include <simple2d.h>
 #include "shift_register.h"
@@ -530,81 +528,11 @@ void free()
 	delete[] stft_content;
 }
 
-void audio_callback(AudioIOData &io)
-{
-	_2009_DaPlSt &beat_tracking = io.user<_2009_DaPlSt>();
-
-	cout << "callback" << endl;
-
-	while (io())
-	{
-		float sample = io.in(0);
-
-		cout << sample << endl;
-		input_samples_max.push(sample);
-		beat_tracking.next(sample);
-	}
-}
-
-void setup_audio_input()
-{
-	AudioDevice::printAll();
-	AudioDevice dev = AudioDevice::defaultInput();
-	_2009_DaPlSt beat_tracking = _2009_DaPlSt(dev.defaultSampleRate());
-	AudioIO io(
-		(int) round(ODF_SAMPLE_INTERVAL * dev.defaultSampleRate()),
-		dev.defaultSampleRate(),
-		&audio_callback,
-		&beat_tracking,
-		0,
-		1
-	);
-
-	io.deviceIn(dev);
-	io.print();
-
-	io.start();
-}
-
-
-// represents the parsed command line args
-struct Args
-{
-	char *executable;
-
-	Args();
-
-	/// Parses the command line arguments.
-	///
-	/// If the arguments cannot be parsed, an error and usage message is printed
-	/// and `executable` is set to NULL.
-	Args(int argc, char **argv);
-};
-
-Args::Args()
-{
-	this->executable = nullptr;
-}
-
-Args::Args(int argc, char **argv)
-{
-	this->executable = argv[0];
-}
-
 
 int demo(int argc, char **argv)
 {
-	Args args = Args(argc, argv);
-
-	if (args.executable == nullptr)
-	{
-		return 1;
-	}
-
 	init();
 	S2D_Show(window);
-	char x;
-	cin >> x;
 	free();
 
 	return 0;
